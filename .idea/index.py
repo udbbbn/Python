bd2095a6c839d1305639785d7f321eb133676af0
udbbbn <udbbbn@163.com>
2016-12-26 20:35:14 +0800

    Default

--- .idea/index.py
+++ .idea/index.py
@@ -36,6 +36,7 @@ for x in range(5):                              #range(5)  =  [1,2,3,4,5]
     sum += x
 print(sum)
 
+"""获取用户输入的字符串"""
 try:                                            #python3.x input等于python2.x 的raw_input
    birth = int(input('birth:'))                 #这里必须加int()否则会报错 因为input()默认获取的是string类型
    if birth < 2000:
@@ -45,6 +46,7 @@ try:                                            #python3.x input等于python2.x 
 except ValueError:
     print("请输入数字")
 
+"""dict set集合"""
 dict = {'Michael':95,'Bob':75,'Tracy':85}       #dict 就是 object
 dict['Tomes'] = 99
 print('Tomes' in dict)                          #用in判断dict中有没有Tomes
@@ -59,3 +61,27 @@ s1 = set([1,2,3])
 s2 = set([2,3,4])
 print (s1 & s2)                                 #可以进行数学上的运算 交集
 print (s1 | s2)                                 #并集
+
+"""数据类型"""
+s = abs(-3*6)                                    #abs 绝对值函数
+print (s)
+s = max(1,2)                                     #max获取最大值
+print (s)
+s = int('1234')
+print (s)
+s = float('1433')
+print (s)
+s = str(321)
+print (s)
+s = bool('')
+print (s)
+s = hex(23)                                      #hex转换成16进制字符串
+print (s)
+
+"""函数"""
+def my_abs(x):                                   #用def来定义函数
+    if x >= 0:
+        return x
+    else:
+        return -x
+print(my_abs(-5))
