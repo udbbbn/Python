47e82a63e7d5e7224ab504d85443f51d0baf1697
udbbbn <udbbbn@163.com>
2017-01-17 18:32:17 +0800

    Default

--- spider.py
+++ spider.py
@@ -1,7 +1,14 @@
 #encoding:UTF-8
+import urllib
 import urllib.request
 
-url = "http://www.baidu.com"
-data = urllib.request.urlopen(url).read()
+data = {}
+data['word'] = 'Jecavay Notes'
+
+url = "http://www.baidu.com/s?"
+url_values = urllib.parse.urlencode(data)
+full_url = url + url_values
+
+data = urllib.request.urlopen(full_url).read()
 data = data.decode('UTF-8')
 print(data)
