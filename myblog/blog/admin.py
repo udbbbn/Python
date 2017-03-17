1a9e8de30c7a6df4115e651aca58bc4940da6a10
udbbbn <udbbbn@163.com>
2017-03-17 14:57:35 +0800

    Default

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from . import models
 
+
 # Register your models here.
 
-admin.site.register(models.Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'pub_time')
+    list_filter = ('pub_time',)
+
+
+admin.site.register(models.Article, ArticleAdmin)
--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=32, default='Title')
     content = models.TextField(null=True)
+    pub_time = models.DateField(null=True)
 
     def __str__(self):
         return self.title
