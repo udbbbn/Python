83f45bf1a774163ba156c0761a2355ef7a92f32f
udbbbn <udbbbn@163.com>
2017-10-09 11:08:24 +0800

    update 2017-10-09

--- Email/cgi-bin/handle.py
+++ Email/cgi-bin/handle.py
@@ -6,7 +6,7 @@
 import cgi, cgitb
 import random
 
-from module_email_txt import module_email #编辑器未识别 实际可用
+from module_email import module_email_txt #编辑器未识别 实际可用
 cgitb.enable()#使错误信息显示在网页上
 
 
@@ -21,7 +21,7 @@ title = '验证码'
 
 msg = [str(code), '郑和保险']
 
-value = module_email(recever, title, msg)#调用email发送函数
+value = module_email_txt(recever, title, msg)#调用email发送函数
 
 print ("Content-type: text/html")#告诉python 现在输出的是html信息
 print('')#区分页头与内容
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 python 自学项目
 
 索引
-Email--------用户注册邮箱验证  ajax email python-cgi编程
+Email--------用户注册邮箱验证  ajax email python-cgi编程   邮箱发送附件 图片
 Excel--------excel表导入到数据库 mysql
 Save_File----上传文件 及 文件下载
 itchat-------微信机器人
